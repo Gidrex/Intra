6f0384aa47a49c7e3f4816def05ca6f22fd54c50
Gidrex <Desench@proton.me>
2024-05-22 21:54:58 +0300

    adding server-side for registration/login

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./database');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/reg.html', (req, res) => {
+  res.sendFile(__dirname + '/templates/reg.html');
+});
+
+app.post('/register', (req, res) => {
+  const { username, email, password } = req.body;
+  const stmt = db.prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+  stmt.run(username, email, password, (err) => {
+    if (err) {
+      res.send('Registration failed');
+    } else {
+      res.send('Registration successful');
+    }
+  });
+  stmt.finalize();
+});
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(__dirname + '/templates/login.html');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  db.get("SELECT * FROM users WHERE email = ? AND password = ?", [email, password], (err, row) => {
+    if (err) {
+      res.send('Login failed');
+    } else if (row) {
+      res.send('Login successful');
+    } else {
+      res.send('Invalid email or password');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
